d306a966732a1093451e35151e1b58180e5878d3
prynge <37028634+prynge@users.noreply.github.com>
2021-10-14 19:24:18 +0200

    token

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,7 +1,7 @@
 export default function authHeader() {
   let user = JSON.parse(localStorage.getItem('user'));
-  if (user && user.token) {
-    return { 'Authorization': 'Bearer '+user.token, "Access-Control-Allow-Origin":"*" };
+  if (user && user) {
+    return { 'Authorization': 'Bearer '+user, "Access-Control-Allow-Origin":"*" };
   } else {
     return {"Access-Control-Allow-Origin":"*", "Content-type": "application/json"};
   }
